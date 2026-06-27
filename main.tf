dc23c2cd64f841b939bcf5f6143e14c130ce3d37
Nikita Ivanichenko <importantthings1482@gmail.com>
2026-06-27 23:14:50 +0300

    Solution

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.105.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name   = "myTFStateResourceGroup"
+    storage_account_name  = "nichelangeloostorage"
+    container_name        = "tfstate"
+    key                   = "terraform.tfstate"
+    use_oidc = true
+  }
+}
+
+provider "azurerm" {
+  features {}
+  use_oidc = true
+}
